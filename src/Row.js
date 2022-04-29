c1d3f96f65e563aa4193d4284e01591b8a9a2028
Harsh <harshjhala0309@gmail.com>
2022-04-29 21:30:12 +0530

    adding files

---
+++ src/Row.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
+  // A snippet of code which runs based on a specific condition
+  // what we are trying to achieve here is when our row component load we can use your code like we have to make a request to tmdb to run this
+
+  useEffect(() => {
+    // if[], run once when the row loads, and don't run again
+    // this will only run on page load, if we pass an argument then everytime it will load
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      // 'https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213',
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      //  https:/developers.google.com/youtube/player_parameters
+
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      {/* title */}
+      {/* conatiner-> bunch of posters inside it */}
+
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            onClick={() => handleClick(movie)}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+    </div>
+  );
+}
+
+export default Row;
